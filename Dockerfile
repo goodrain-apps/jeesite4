7d096b9cc1229b598aa3be097307b277ed4050be
guox <guox@goodrain.com>
2018-06-28 12:50:33 +0800

    [update] first commit

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
-#
+FROM goodrainapps/tomcat:7.0.82-jre7-alpine
+
+COPY docker-entrypoint.sh /
+
+RUN chmod +x /docker-entrypoint.sh
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
+CMD ["catalina.sh", "run"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,1 +1,38 @@
-#
+#!/bin/bash
+
+[[ $DEBUG ]] &&  set -x
+
+[[ $PAUSE ]] && sleep $PAUSE
+
+if [[ ! -f ./webapps/mogocloud-${VERSION:-gejie}.war ]];then
+  wget -q https://pkg.goodrain.com/apps/jeesite4/jeesite4.war -O /usr/local/tomcat/webapp/ROOT.war
+fi
+
+
+# detect ENABLE_APM env 通过环境变量开启pinpoint的APM监测（基础镜像自带的功能）
+if [ "$ENABLE_APM" == "true" ];then
+    COLLECTOR_IP=${COLLECTOR_IP:-127.0.0.1}
+    sed -i 's/${PINPOINT_AGETN_VERSION}.jar/${PINPOINT_AGETN_VERSION}-SNAPSHOT.jar/g' /usr/local/tomcat/bin/pinpoint-agent.sh
+    sed -i "2 a. /usr/local/tomcat/bin/pinpoint-agent.sh" /usr/local/tomcat/bin/catalina.sh
+    sed -i -r -e "s/(profiler.collector.ip)=.*/\1=${COLLECTOR_IP}/" \
+              -e "s/#(profiler.applicationservertype=TOMCAT)/\1/" /usr/local/pinpoint-agent/pinpoint.config
+fi
+
+
+# redis session 通过环境变量开启redis session功能（基础镜像自带的功能）
+if [ "$REDIS_SESSION" == "true" ];then
+    sed -i 's#</Context>##' /usr/local/tomcat/conf/context.xml
+
+cat >>  /usr/local/tomcat/conf/context.xml << END
+<Valve className="com.orangefunction.tomcat.redissessions.RedisSessionHandlerValve" />
+<Manager className="com.orangefunction.tomcat.redissessions.RedisSessionManager"
+             host="127.0.0.1" 
+             port="6379" 
+             database="0" 
+             maxInactiveInterval="60"  />
+</Context>
+END
+
+fi
+
+exec $@
